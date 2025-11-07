d629649167ab9d0f9436905dbc97aaf0fbb209f9
Benjamin Lizama <be.lizamac@duocuc.cl>
2025-11-06 21:42:07 -0300

    feat: Implementa MS carrito al proyecto

---
+++ carrito/src/test/java/com/level_up/carrito/CarritoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.level_up.carrito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarritoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
