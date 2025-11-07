2f5c20c40b3f8d6ec4e44a18b0b5ad1fa1218a33
Benjamin Lizama <be.lizamac@duocuc.cl>
2025-11-06 21:47:46 -0300

    feat: Implementa MS Productos

---
+++ productos/src/test/java/com/level_up/productos/ProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.level_up.productos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
