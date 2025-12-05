e094f9fce0094d77384f719f2c66aae1200d9e99
Lucciano Martinez <lucc.martinez@duocuc.cl>
2025-12-05 15:28:16 -0300

    test: pruebas de integracion a carrito controller

--- carrito/src/test/java/com/level_up/carrito/controller/CarritoControllerTest.java
+++ carrito/src/test/java/com/level_up/carrito/controller/CarritoControllerTest.java
@@ -1,16 +1,28 @@
 package com.level_up.carrito.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.level_up.carrito.client.ProductoFeignClient;
 import com.level_up.carrito.dto.AgregarItemDTO;
+import com.level_up.carrito.dto.ProductoExternoDTO;
+import com.level_up.carrito.repository.CarritoRepository;
+import com.level_up.carrito.service.CarritoService;
 import lombok.With;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.*;
 
 @org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase(replace = org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.ANY)
 @SpringBootTest
@@ -24,25 +36,49 @@ class CarritoControllerTest {
     private MockMvc mockMvc;
     @Autowired
     private ObjectMapper objectMapper;
+    @Autowired
+    private CarritoService carritoService;
+    @Autowired
+    private CarritoRepository carritoRepository;
 
-    @Test
-
-    void agregarItem() {
-        AgregarItemDTO itemDTO = new AgregarItemDTO();
-        itemDTO.setCantidad(10);
-        itemDTO.setCodigoProducto("PROD_1");
+    @MockBean
+    private ProductoFeignClient productoFeignClient;
+    @BeforeEach
+    void setUp(){
+        carritoRepository.deleteAll();
 
     }
 
     @Test
-    void obtenerCarrito() {
-    }
+    void agregarItem() throws Exception{
+
+        carritoService.inicializarCarrito(1L);
+
+        AgregarItemDTO agregarItemDTO = new AgregarItemDTO();
+        agregarItemDTO.setCodigoProducto("1122");
+        agregarItemDTO.setCantidad(1);
+
+        String requestContent = objectMapper.writeValueAsString(agregarItemDTO);
+
+        mockMvc.perform(post("/api/carritos/{id}", 1).contentType(MediaType.APPLICATION_JSON).content(requestContent)).andExpect(status().isForbidden());
+
+    };
+
+
 
     @Test
-    void eliminarItem() {
+    void obtenerCarrito() throws Exception {
+        mockMvc.perform(get("/api/carritos/{id}", 1)).andExpect(status().isForbidden());
+
+    }
+    @Test
+    @WithMockUser(roles = "ADMIN",username = "ADMIN")
+    void obtenerCarritoAdmin() throws Exception {
+        carritoService.inicializarCarrito(1L);
+        mockMvc.perform(get("/api/carritos/{id}", 1)).andExpect(status().isOk());
+
     }
 
-    @Test
-    void vaciarCarritoDelUsuario() {
-    }
+
+
 }
